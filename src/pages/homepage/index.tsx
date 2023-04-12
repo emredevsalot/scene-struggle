2cc2597ca914c42b9881c7103857ba395173b1a8
Emre Ergül <emredevsalot@gmail.com>
2023-04-12 18:35:14 +0300

    changed games path parameter from channel id to channel title

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -73,7 +73,7 @@ const Homepage = (props: Props) => {
         {/* {channelId && <p>Channel Id: {channelId}</p>} */}
         {channel.title && <p className="text-5xl">{channel.title}</p>}
         {channel.id && (
-          <Link to={`/games/${channel.id}`} state={{ channel }}>
+          <Link to={`/games/${channel.title}`} state={{ channel }}>
             <Button>{`Go to games about ${channel.title}`}</Button>
           </Link>
         )}
